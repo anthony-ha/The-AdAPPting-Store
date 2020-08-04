0db23ebbc2b1dfe6251086974a63d3fa9f00cca2
Anthony <anthony.ha07@gmail.com>
2020-08-04 15:53:11 -0400

    Implemented logistics module

--- logistics-module/client/src/components/Info.jsx
+++ logistics-module/client/src/components/Info.jsx
@@ -4,24 +4,22 @@ import Rating from './Rating.jsx';
 import AdditionalInfo from './AdditionalInfo.jsx';
 
 const Info = function(props) {
-  var editorsChoiceLogo = 'https://icons-987654321.s3-us-west-1.amazonaws.com/editchoicefull.png';
-  var editorsChoice = 'Editors\' Choice';
+  var editorsChoiceLogo = '';
+  var editorsChoice = '';
 
   if (props.app.editorChoice === true) {
-    editorsChoiceLogo = 'https://icons-987654321.s3-us-west-1.amazonaws.com/editchoicefull.png';
+    editorsChoiceLogo = 'https://lh3.googleusercontent.com/HotsP0KmK4tn0Q8p9szRXtwjD7fZRKu4mFcfJUFoddrGiZefxY7gz4dEGMuH6HsfCymJP6a8MvAwYWrU=s14-rw';
     editorsChoice = " Editors' Choice";
   }
 
   return (
     <div className='info'>
-      <img className='image' src='https://icons-987654321.s3-us-west-1.amazonaws.com/stonefruitlogo.jpg'></img>
-      <div className='name'>AdAPPting Store</div>
-      <div className='editorsChoice'> <img src={editorsChoiceLogo}></img></div>
-      <div className='author'>Anthony Ha</div>
-      <div className='category'> Productivity</div>
-
-      <div className='ads'>      <img className='everyone' src='https://icons-987654321.s3-us-west-1.amazonaws.com/everyone-rating.png'></img><div className='contains'>Contains Ads  ·  Offers in-app purchases</div><img className='caution' src='https://w1.pngwave.com/png/693/31/638/warning-icon-red-triangle-sign-line-signage-traffic-sign-symbol-png-clip-art.png'></img>You don't have any devices.
-</div>
+      <img className='image' src='https://i.picsum.photos/id/403/100/100.jpg?hmac=u69eaQcW-fHFQXgCTwIlurxwv6IjJLNxlW_RDazTuaA'></img>
+      <div className='name'>{props.app.name}</div>
+      <div className='editorsChoice'> <img src={editorsChoiceLogo}></img> {editorsChoice}</div>
+      <div className='author'>{props.app.author}</div>
+      <div className='category'> {props.app.category}</div>
+      <div className='ads'><div className='contains'>Contains Ads</div><img className='caution' src='https://w1.pngwave.com/png/693/31/638/warning-icon-red-triangle-sign-line-signage-traffic-sign-symbol-png-clip-art.png'></img>You don't have any devices.</div>
       <div className='wishlist'><img className='wishlistLogo' src='https://cdn0.iconfinder.com/data/icons/mix-of-simple-vol-4/57/02-512.png'></img>Add to Wishlist</div>
       <button className='install-button'>Install</button>
       <Rating rating={props.app.rating} ratings={props.app.ratings}/>
